7dbd588bc7a5092be8c7cf926072b1a0fd652fad
aleksandrzverev <aleksanderzverev.ru@gmail.com>
2018-06-02 11:50:20 +0300

    Revert "задание материала"
    This reverts commit 1fd430e840fae5fd4d26d7da5fde963abae65bd2.

--- poisson.py
+++ poisson.py
@@ -12,18 +12,6 @@ Find :math:`t` such that:
 """
 from __future__ import absolute_import
 from sfepy import data_dir
-material={
-   'coef':({'val':1.0},),
-}
-fields={
-'temperature':('real',1,'Omega',1),
-}
-equations={
-'Temperature':"""dw_laplace.i.Omega(coef.val, s,t)=0"""
-}
-integrals={
-'i':2,
-}
 
 filename_mesh = data_dir + '/meshes/3d/cylinder.mesh'
 
